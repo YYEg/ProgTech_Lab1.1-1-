cab8ffefdd3a11989865a8e128c7a1b5aeef3a00
YuEgor <rezervdesu15@gmail.com>
2022-09-06 18:09:40 +0800

    Добавьте файлы проекта.

---
+++ ConsoleApp1Tests/LogicTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FirstApplication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApplication.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        public void SquareSMoreThanCircleS()
+        {
+            var Circle_S = Logic.Circle_area(3);
+            var Square_S = Logic.Square_area(7);
+
+            var OutMessage = Logic.Compare(Square_S, Circle_S);
+
+            Assert.AreEqual("Площадь квадрата больше площади круга!", OutMessage);
+        }
+
+        [TestMethod()]
+        public void CircleSMoreThanSquareS()
+        {
+            var Circle_S = Logic.Circle_area(5);
+            var Square_S = Logic.Square_area(6);
+
+            var OutMessage = Logic.Compare(Square_S, Circle_S);
+
+            Assert.AreEqual("Площадь круга больше площади квадрата!", OutMessage);
+        }
+    }
+}
